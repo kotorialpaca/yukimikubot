33a15e9a84c595d8122b2588b8650ce7136db0a7
Subeen Chung <subeen.chung@g4box.local>
2017-02-13 14:15:46 -0500

    test

--- services/event.go
+++ services/event.go
@@ -116,6 +116,7 @@ func (e Event) GroupsToString() string {
 	} else {
 		outstr = "**[Members]**\n"
 		for _, b := range e.Groups[0].Members {
+			fmt.Println(b.Nick)
 			outstr = outstr + b.Nick + "\n"
 		}
 	}
