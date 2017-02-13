11e918c0334d1c6a5ebb00aa28d0348136d68aaf
Subeen - Home <subeen@home>
2017-02-12 23:26:03 -0500

    www

--- services/event.go
+++ services/event.go
@@ -6,14 +6,20 @@ import (
 
 	"fmt"
 
+	"strconv"
+
+	"errors"
+
 	"github.com/bwmarrin/discordgo"
 )
 
+//EventGroup object which groups all the Event objects
 type EventGroup struct {
 	Name   string
 	Events []Event
 }
 
+//Event object to represent events for the Discord chat
 type Event struct {
 	Name      string
 	Author    discordgo.Member
@@ -25,12 +31,14 @@ type Event struct {
 	EndDate   time.Time
 }
 
+//Group object for storing groups split within an Event object
 type Group struct {
 	Name      string
 	Members   []discordgo.Member
 	MaxMember int
 }
 
+//NewEvent returns a new Event object
 func NewEvent(n string, d string, sd string, ed string, u discordgo.Member, m int) *Event {
 	layout := "2006-01-02 03:04PM"
 	//Start Time in Time variable
@@ -42,6 +50,7 @@ func NewEvent(n string, d string, sd string, ed string, u discordgo.Member, m in
 	if err != nil {
 		fmt.Println("welp something went wrong while creating an event")
 	}
+
 	return &Event{
 		Name:      n,
 		Desc:      d,
@@ -52,6 +61,7 @@ func NewEvent(n string, d string, sd string, ed string, u discordgo.Member, m in
 	}
 }
 
+//NewGroup returns a new Group object within Event
 func NewGroup(n string, max int) *Group {
 	return &Group{
 		Name:      n,
@@ -87,27 +97,36 @@ func (e Event) PrintPrettyString() string {
 	outstr = strings.Replace(outstr, "EVT_NAME", e.Name, -1)
 	outstr = strings.Replace(outstr, "AUTHOR", e.Author.Nick, -1)
 	outstr = strings.Replace(outstr, "DESC", e.Desc, -1)
-	outstr = strings.Replace(outstr, "CUR_MEM", string(len(e.Members)), -1)
-	outstr = strings.Replace(outstr, "MAX_MEM", string(e.MaxMember), -1)
+	outstr = strings.Replace(outstr, "CUR_MEM", strconv.Itoa(len(e.Members)), -1)
+	outstr = strings.Replace(outstr, "MAX_MEM", strconv.Itoa(e.MaxMember), -1)
 
 	return outstr
 }
 
+//GroupsToString returns string representation of the groups
 func (e Event) GroupsToString() string {
 	outstr := ""
 	if len(e.Groups) != 0 {
-		for a, b := range e.Groups {
+		for _, b := range e.Groups {
 			outstr = outstr + "**[" + b.Name + "]**\n"
-			for i, v := range b.Members {
+			for _, v := range b.Members {
 				outstr = outstr + "*" + v.Nick + "*\n"
 			}
 		}
 	} else {
 		outstr = "**[Members]**\n"
-		for a, b := range e.Members {
+		for _, b := range e.Members {
 			outstr = outstr + "**[" + b.Nick + "]**\n"
 		}
 	}
 
 	return outstr
+}
+
+func (e Event) addToGroup(gn string, m discordgo.Member) error {
+	return errors.New("not yet implemented")
+}
+
+func (e Event) addToEvent(m discordgo.Member) error {
+	return errors.New("not yet implemented")
 }
